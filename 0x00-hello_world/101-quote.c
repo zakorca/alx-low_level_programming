210cd7e5fba0dce08326750bee7ca803c5101704
zakorca <zakariatouras47.com>
2023-09-11 19:41:42 -0700

    task adv 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
