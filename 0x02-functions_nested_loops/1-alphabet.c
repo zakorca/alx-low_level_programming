c1d64f1b49857af1fde157feb05b5180b274d38e
zakorca <zakariatouras47.com>
2023-09-12 18:23:51 -0700

    task1:low alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_alphabet - function to write alphabet in lower case
+ *
+ * Return : 0
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i ='a'; i <= 'z'; i++ )
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
