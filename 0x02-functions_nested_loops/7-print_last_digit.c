47a6ef74ccdf88db408a7ca52b47bec14001f2c9
zakorca <zakariatouras47.com>
2023-09-15 19:31:27 -0700

    task7 not fixed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit number
+ * @n: the number to check
+ * Return: value of last number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
