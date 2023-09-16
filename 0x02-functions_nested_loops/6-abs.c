28daf608154bc6df8a2465e5b204a6c465bc5a25
zakorca <zakariatouras47.com>
2023-09-15 19:14:08 -0700

    task7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _abs - function calculate absolute value
+ * @n: the number
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+
+}
