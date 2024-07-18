fc4fcea7f243aaf94df734195474fe77ff978c3a
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-07-18 11:03:30 -0700

    task 100

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,19 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
+
 int binary_search(int *array, size_t size, int value);
+void printarr(int *array, size_t start, size_t end);
+size_t binary_help(int *array, int value, size_t start, size_t end);
+
 int jump_search(int *array, size_t size, int value);
+
+int interpolation_search(int *array, size_t size, int value);
+size_t prob(int *array, int value, size_t low, size_t high);
+
+int exponential_search(int *array, size_t size, int value);
 
 #endif
