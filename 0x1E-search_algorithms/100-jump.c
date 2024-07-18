a1d073033d4d41666bc1b0fdc8b9891fd79e1d53
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-07-18 11:24:21 -0700

    task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,31 +1,50 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_search - shearch using the dictionary technic
- * @array: the list
- * @size: the size of the list
- * @value: the value looking for
- * Return: the index if success -1 when failed
+ * jump_search - searches for a value in a sorted array of
+ * integers using the Jump search algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: return the first index where value is located or -1
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-	size_t m = (size_t)sqrt(size), i = 0;
+	size_t step = 0, prev_step = 0;
 
 	if (!array)
 		return (-1);
-	while (i < size && array[i] < value)
+	/* search the block */
+	while (array[step] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		i += m;
+		printf("Value checked array[%ld] = [%d]\n", step,
+				array[step]);
+		prev_step = step;
+		step += sqrt(size);
+		if (step >= size)
+			break;
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n", i - m, i);
-	i -= m;
-	while (i < size && array[i] <= value)
+	printf("Value found between indexes [%ld] and [%ld]\n", prev_step,
+			step);
+	/* make a linear search inside the found block */
+	while (array[prev_step] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
+		printf("Value checked array[%ld] = [%d]\n", prev_step,
+				array[prev_step]);
+		prev_step++;
+		if (prev_step >= size)
+			return (-1);
+	}
+	/* check that the value found */
+	if (array[prev_step] == value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", prev_step,
+				array[prev_step]);
+		return (prev_step);
 	}
 	return (-1);
 }
